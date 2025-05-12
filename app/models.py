92095483d6f8c17cf907cf0a9872a278cdfa67ee
issmuth <ismael.mdev@gmail.com>
2025-05-12 18:32:48 +0300

    feat(product): add product routes

--- app/models.py
+++ app/models.py
@@ -11,7 +11,7 @@ class BaseModel(me.Document):
 
     def get_serialized(self):
         data = self.to_mongo().to_dict()
-        data['id'] = str(data.pop('_id'))
+        data["id"] = str(data.pop("_id"))
         return data
 
     @classmethod
@@ -34,8 +34,8 @@ class User(BaseModel):
     otp = me.StringField()
     otp_expiry = me.FloatField()
     role = me.StringField()
-    shop = me.ReferenceField('Shop') #for employees
-    shops = me.ListField(me.ReferenceField('Shop')) #for owners
+    shop = me.ReferenceField('Shop') 
+    shops = me.ListField(me.ReferenceField('Shop')) 
     isVerified = me.BooleanField(required=True, default=False)
     
     meta = {'collection': 'users'}
@@ -95,8 +95,7 @@ class User(BaseModel):
     
     @classmethod
     def get_employees_by_shop_id(cls, shop_id):
-        return cls.objects(shop=shop_id)
-    
+        return cls.objects(shop=shop_id)    
 
 
 """
@@ -130,10 +129,22 @@ class Product(BaseModel):
     image_url = me.StringField(default="")
 
     meta = {'collection': 'products'}
+
+    def get_serialized(self):
+        data = self.to_mongo().to_dict()
+        data["id"] = str(data.pop("_id"))
+        data["shop_id"] = str(data.pop("shop"))
+        return data
+
+
     @classmethod
     def get_product_by_id(cls, id):
         return cls.objects(id=id).first()
 
+
+"""
+Item Model
+"""
 class Item(BaseModel):
     product = me.ReferenceField(Product, required=True, reverse_delete_rule=me.CASCADE)
     barcode = me.StringField(unique=True)
@@ -156,21 +167,28 @@ class Item(BaseModel):
         return cls.objects(barcode=barcode)
 
 
+
+"""
+Payload embeded document definition
+"""
+class ProductPayload(me.EmbeddedDocument):
+    product_id = me.StringField(required=True)
+    name = me.StringField(required=True)
+    category = me.StringField(required=True)
+    quantity = me.IntField(required=True)
+    price = me.FloatField(required=True)
+    isSerialized = me.BooleanField(default=False)
+    barcodes = me.ListField(me.StringField())
+
+"""
+Transaction Model
+"""
 class Transaction(BaseModel):
     date = me.DateTimeField(default=datetime.utcnow)
     shop = me.ReferenceField(Shop, required=True)
     user = me.ReferenceField(User, required=True)
     transaction_type = me.StringField(choices=["sale", "restock"], required=True)
-    products = me.DictField(required=True)
+    payload = me.ListField(me.EmbeddedDocumentField(ProductPayload), required=True)
     total = me.FloatField(required=True)
-
-    def save(self, *args, **kwargs):
-        self.total = sum(
-            product.price * quantity
-            for productId, quantity in self.products.items()
-            for product in Product.objects(id=productId)
-        )
-
-        super().save(*args, **kwargs)
 
     meta = {'collection': 'transactions'}
